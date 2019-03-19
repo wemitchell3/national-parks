03f20813ef833af5bec60ad71af940f695802371
William Mitchell <wemitchell3@gmail.com>
2019-03-19 10:41:16 -0500

    fix trailing space

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -7,7 +7,7 @@ const getNationalParks = () => {
 //This is the function call since there is not a call for it in the dom manager.
 getNationalParks()
 
-//Function to fetch the weather for the parks using their lat long coords in the getNationalParks function 
+//Function to fetch the weather for the parks using their lat long coords in the getNationalParks function
 const getNationalParksWeather = (latitude, longitude) => {
   return fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/0ca9b8198755c7e5839fa4e60a0bb6cc/${latitude},${longitude}`)
     .then(response => response.json())
