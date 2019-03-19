077451e2e0073a9fba8bf0228115d0f7061b0178
William Mitchell <wemitchell3@gmail.com>
2019-03-19 10:38:50 -0500

    Uploading minor revisions to code. Notes and clean up.

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -1,12 +1,14 @@
+//Function to fetch parks api data. This JSON is located within C:\Users\William\workspace\national-parks
 const getNationalParks = () => {
   return fetch("http://localhost:9099/parks")
     .then(r => r.json())
     .then(parsedParks => renderParkElements(parsedParks))
 };
+//This is the function call since there is not a call for it in the dom manager.
 getNationalParks()
 
+//Function to fetch the weather for the parks using their lat long coords in the getNationalParks function 
 const getNationalParksWeather = (latitude, longitude) => {
   return fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/0ca9b8198755c7e5839fa4e60a0bb6cc/${latitude},${longitude}`)
     .then(response => response.json())
   };
-// getNationalParksWeather()
